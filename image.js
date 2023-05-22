02b2705881c6d744351af8578d51f2f01068c76c
unknown <palaksinghbais.29@gmail.com>
2023-05-22 17:47:39 +0530

    all set

--- image.js
+++ image.js
@@ -24,12 +24,15 @@ const searchImages = async (e) => {
     // console.log(result.links.html);
     resultEle.innerHTML += `
         <div
-            class="image-box mb-14 shadow-lg rounded overflow-hidden w-full text-center sm:w-3/5 md:w-2/5 xl:w-1/4 2xl:w-1/4">
+            class="image-box mb-14 shadow-lg rounded py-4 overflow-hidden w-full text-center sm:w-3/5 md:w-2/5 xl:w-1/4 2xl:w-1/4">
             <img class="w-full xl:h-56 md:h-56 object-cover rounded hover:scale-105 transition-all duration-100 delay-100 ease-in-out" src="${result.urls.small}" alt="${result.alt_description}">
+            <p id="p-tag" class="py-2 text-xl"><span class="text-orange-600 font-bold">Description:</span> ${result.alt_description}</p>
             <a class="p-4 block text-gray-600 transition-all duration-100 delay-100 ease-in-out hover:bg-gray-200"
                 href="${result.links.html}" target="_blank">More
                 details</a>
+                <a href = ${result.links.download} target="_blank" class="bg-blue-400 px-4 text-white cursor-pointer hover:bg-blue-600 rounded py-0.5 uppercase">download</a>
         </div>`;
+    console.log(result);
   });
   page++;
   console.log(page);
